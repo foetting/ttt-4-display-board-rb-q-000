7a4530ac433bf8c568984c5d3b3809e2302c39e2
foetting <florian.oettinger@gmail.com>
2016-05-04 18:55:36 +0000

    nitrous-backup at Wed May  4 18:55:36 UTC 2016

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,25 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = [" ","X","O"]
+def display_board
+  print "#{board(0)}"
+  print "|"
+  print "#{board(1)}"
+  print "|"
+  puts "#{board(2)}"
+  print "-----------"
+  puts ""
+  print "#{board(0)}"
+  print "|"
+  print "#{board(0)}"
+  print "|"
+  puts "#{board(0)}"
+  print "-----------"
+  puts ""
+  print "#{board(0)}"
+  print "|"
+  print "#{board(0)}"
+  print "|"
+  puts "#{board(0)}"
+end
+
+
+
